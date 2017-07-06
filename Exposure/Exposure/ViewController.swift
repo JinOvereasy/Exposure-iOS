34112aec6caae24cad5bfdfa3c0694feff2d7e6f
ShenTungHuang <shentunghuang@gmail.com>
2017-07-06 11:59:15 -0700

    commit by STH
    with two slider

--- Exposure/Exposure/ViewController.swift
+++ Exposure/Exposure/ViewController.swift
@@ -20,18 +20,29 @@ class ViewController: UIViewController {
     
     var image_1: UIImage?
     var image_2: UIImage?
+    var image_temp1: UIImage?
+    var image_temp2: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view, typically from a nib.
         testImageView.image = try! UIImage(data: Data(contentsOf: URL(string: "https://firebasestorage.googleapis.com/v0/b/makestagram-45567.appspot.com/o/images%2Fposts%2FeoAn4vW2JvUMe89dbV8vRgkn7lt2%2F2017-07-02T00:47:16Z.jpg?alt=media&token=dd1e2f4d-846b-46e9-8f6d-70245d7e4acf")!))
-        self.image_1 = self.testImageView.image
+        var imageSize = (self.testImageView.image?.size)
+        var tempSize = imageSize
+        tempSize?.width = (tempSize?.width)! / 5
+        tempSize?.height = (tempSize?.height)! / 5
+        self.image_1 = self.testImageView.image?.af_imageScaled(to: tempSize!)
 
         testImageView.image = try! UIImage(data: Data(contentsOf: URL(string: "https://firebasestorage.googleapis.com/v0/b/makestagram-45567.appspot.com/o/images%2Fposts%2FjCRXkHD9p4YqtIdSGpEEA2wkLWL2%2F2017-07-02T00:47:43Z.jpg?alt=media&token=afc50129-3aca-4750-bb9a-47f586d1ceb4")!))
-        self.image_2 = self.testImageView.image
+        imageSize = (self.testImageView.image?.size)
+        tempSize = imageSize
+        tempSize?.width = (tempSize?.width)! / 5
+        tempSize?.height = (tempSize?.height)! / 5
+        self.image_2 = self.testImageView.image?.af_imageScaled(to: tempSize!)
         
-        testImageView.image = nil
+        let image_temp1 = modifyAlpha(image_1, in_alpha: 0.5)
+        let image_temp2 = modifyAlpha(image_2, in_alpha: 0)
+        self.testImageView.image = mergedImageWith(frontImage: image_temp1, backgroundImage: image_temp2)
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,13 +50,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /* modify exposure for show */
-    @IBAction func sliderMove(_ sender: UISlider) {
-        
-        let image_temp1 = modifyAlpha(image_1, in_alpha: sender.value)
-        let image_temp2 = modifyAlpha(image_2, in_alpha: sender.value)
-        self.testImageView.image = mergedImageWith(frontImage: image_temp1, backgroundImage: image_temp2)
+    /* first slider move */
+    @IBAction func firstSliderMove(_ sender: UISlider) {
+        image_temp1 = modifyAlpha(image_1, in_alpha: sender.value)
+        makepicture()
+    }
 
+    
+    /* second slider move */
+    @IBAction func sliderMove(_ sender: UISlider) {
+        image_temp2 = modifyAlpha(image_2, in_alpha: sender.value)
+        makepicture()
+    }
+    
+    /* make picture */
+    func makepicture() {
+        self.testImageView.image = mergedImageWith(frontImage: image_temp1, backgroundImage: image_temp2)
     }
     
     /* save image to lib */
